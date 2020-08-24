fbb5dbed17af4296aa4a19d5dbf707e2cd18b262
Aleksandar Stojkovski <aleksandarstojkovski1@gmail.com>
2020-08-24 16:04:52 +0200

    Updating git pages

---
+++ build/precache-manifest.a2b8a34d9650fb14c6401ba5263b9547.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97e06801dfcd3c624e78ce53350b6da",
+    "url": "/my-cv/index.html"
+  },
+  {
+    "revision": "435c8ef96d44a1fbc83d",
+    "url": "/my-cv/static/js/2.34fbec52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-cv/static/js/2.34fbec52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad6bc23086a6e30ec678",
+    "url": "/my-cv/static/js/main.75ae5023.chunk.js"
+  },
+  {
+    "revision": "59b714c33bdd7cb64d9c",
+    "url": "/my-cv/static/js/runtime-main.c75d1202.js"
+  },
+  {
+    "revision": "1315623f796faf26f596b8c815d546bf",
+    "url": "/my-cv/static/media/Semos-andoid.1315623f.png"
+  },
+  {
+    "revision": "fb55ff5db09669f8af342b5fdda4bf5d",
+    "url": "/my-cv/static/media/UC-dca19b40-3931-4d5d-a27d-316d632c369f.fb55ff5d.png"
+  },
+  {
+    "revision": "1c280c5439b4fab54f9e709663f183e5",
+    "url": "/my-cv/static/media/aleks_profile.1c280c54.png"
+  },
+  {
+    "revision": "29b00452bc214a7db2b81a6d2b0e9678",
+    "url": "/my-cv/static/media/backend-01.29b00452.png"
+  },
+  {
+    "revision": "30273d8bd410f088c8d4c8ece7eac461",
+    "url": "/my-cv/static/media/backend-02.30273d8b.png"
+  },
+  {
+    "revision": "1629484341769cf0cfb84edd2353e286",
+    "url": "/my-cv/static/media/drums-site.16294843.png"
+  },
+  {
+    "revision": "23885f6804fc1d76c24a25a3cd60b90f",
+    "url": "/my-cv/static/media/keeper_app.23885f68.png"
+  },
+  {
+    "revision": "8eb4d883633147645c7ce502b361b9d6",
+    "url": "/my-cv/static/media/newsletter-app.8eb4d883.png"
+  },
+  {
+    "revision": "d8cf77403db1c802576acc37627ff00b",
+    "url": "/my-cv/static/media/react-brands.d8cf7740.svg"
+  },
+  {
+    "revision": "52bece77ac8537e9d52cbfc1efb970c6",
+    "url": "/my-cv/static/media/simons-game.52bece77.png"
+  },
+  {
+    "revision": "4d89c01486aa181111a0084013f13cf1",
+    "url": "/my-cv/static/media/tindog-site.4d89c014.png"
+  },
+  {
+    "revision": "b4b15a70579429d5a7056bd7baa5346d",
+    "url": "/my-cv/static/media/todolist-app.b4b15a70.png"
+  }
+]);
