626988faa9a1ec787ae4ba61f669f64a7fb2dbbf
Aleksandar Stojkovski <aleksandarstojkovski1@gmail.com>
2020-08-25 10:58:59 +0200

    Change to JSON and add modules

---
+++ build/precache-manifest.1700dd5b5d1a37a444d865928d0c6990.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c8b23648b5536b5835981bb9b1418d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "840fda192045e724b523",
+    "url": "/static/css/main.ae5e2595.chunk.css"
+  },
+  {
+    "revision": "435c8ef96d44a1fbc83d",
+    "url": "/static/js/2.34fbec52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.34fbec52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "840fda192045e724b523",
+    "url": "/static/js/main.9621b247.chunk.js"
+  },
+  {
+    "revision": "5a5021d0364687fd15b0",
+    "url": "/static/js/runtime-main.bb7f2b33.js"
+  },
+  {
+    "revision": "1315623f796faf26f596b8c815d546bf",
+    "url": "/static/media/Semos-andoid.1315623f.png"
+  },
+  {
+    "revision": "fb55ff5db09669f8af342b5fdda4bf5d",
+    "url": "/static/media/UC-dca19b40-3931-4d5d-a27d-316d632c369f.fb55ff5d.png"
+  },
+  {
+    "revision": "1c280c5439b4fab54f9e709663f183e5",
+    "url": "/static/media/aleks_profile.1c280c54.png"
+  },
+  {
+    "revision": "29b00452bc214a7db2b81a6d2b0e9678",
+    "url": "/static/media/backend-01.29b00452.png"
+  },
+  {
+    "revision": "30273d8bd410f088c8d4c8ece7eac461",
+    "url": "/static/media/backend-02.30273d8b.png"
+  },
+  {
+    "revision": "1629484341769cf0cfb84edd2353e286",
+    "url": "/static/media/drums-site.16294843.png"
+  },
+  {
+    "revision": "23885f6804fc1d76c24a25a3cd60b90f",
+    "url": "/static/media/keeper_app.23885f68.png"
+  },
+  {
+    "revision": "8eb4d883633147645c7ce502b361b9d6",
+    "url": "/static/media/newsletter-app.8eb4d883.png"
+  },
+  {
+    "revision": "d8cf77403db1c802576acc37627ff00b",
+    "url": "/static/media/react-brands.d8cf7740.svg"
+  },
+  {
+    "revision": "52bece77ac8537e9d52cbfc1efb970c6",
+    "url": "/static/media/simons-game.52bece77.png"
+  },
+  {
+    "revision": "4d89c01486aa181111a0084013f13cf1",
+    "url": "/static/media/tindog-site.4d89c014.png"
+  },
+  {
+    "revision": "b4b15a70579429d5a7056bd7baa5346d",
+    "url": "/static/media/todolist-app.b4b15a70.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-cv/precache-manifest.4c0685ae6faff649b7b9607e676438e7.js"
+  "/precache-manifest.1700dd5b5d1a37a444d865928d0c6990.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-cv/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
